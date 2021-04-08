cdff97fc76b017eed55b2a85c5f2b8e1ff80117c
unknown <alperensafak@gmail.com>
2021-04-08 23:25:23 +0300

    material-ui import, api connected

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+
+import covidLogo from "./covid-19.svg";
+import { fetchCountries } from "./components/api";
+import AreaChart from "./components/AreaChart";
+
+const useStyles = makeStyles(theme => ({
+  formControl: {
+    margin: "50px auto",
+    width: "50%",
+  },
+}));
+
+const App = () => {
+  const classes = useStyles();
+
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("turkey");
+
+  useEffect(async () => {
+    const countries = await fetchCountries();
+    setCountries(countries);
+  }, []);
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <Grid container>
+     
+          <FormControl className={classes.formControl}>
+            <Select
+              labelId="countries"
+              id="countries"
+              value={country}
+              onChange={event => setCountry(event.target.value)}
+            >
+              {countries.map(country => (
+                <MenuItem key={country.Slug} value={country.Slug}>
+                  {country.Country}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+
+          {/* chart */}
+          <Grid item xs={12}>
+            
+            <AreaChart />
+          </Grid>
+        </Grid>
+      </Container>
+    </React.Fragment>
+  );
+};
+
+export default App;
+
+
+export const functionName = (params) => {
+  
+}
+
---
+++ src/components/api.js
@@ -0,0 +1,14 @@
+import React from "react";
+import axios from "axios";
+
+const baseEndPoint = "https://api.covid19api.com";
+
+export const fetchCountries = async () => {
+  const { data } = await axios.get(`${baseEndPoint}/countries`);
+  return data;
+};
+
+export const fetchDailyData = async country => {
+  const { data } = await axios.get(`${baseEndPoint}/dayone/country/${country}`);
+  return data;
+};
